d953ef997839fd66b1d8d2061d3e38ac00f76f74
Henrique <henriquezambon.s@gmail.com>
2024-03-17 16:54:56 -0300

    fix: include new function

--- M1S06/script.js
+++ M1S06/script.js
@@ -2,3 +2,9 @@ alert('Bem vindo ao meu perfil!')
 
 let newName = document.getElementById('name')
 newName.textContent = 'Henrique, Desenvolvedor Front-end'
+
+const handleMessage = () => {
+  return (
+    alert('Você consegue!')
+  )
+}
