20db40f2f42115be03777538c4a6f7002904db43
Henrique <henriquezambon.s@gmail.com>
2024-04-14 13:47:42 -0300

    fix:

--- M1S08/script.js
+++ M1S08/script.js
@@ -2,3 +2,10 @@ function changeColor(newColor) {
   let element = document.getElementById('title')
   element.style.color = newColor
 }
+
+function changeText(newText) {
+  let elements = document.querySelectorAll('.text p')
+  elements.forEach(element => {
+    element.textContent = newText
+  })
+}
